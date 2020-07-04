d6fde43a24163cd31186c4181cc4bbb8cf47a796
Ravi Kiran Kanculakunta <kancravi@us.ibm.com>
2020-07-04 15:22:03 -0400

    fixed failed deployment

--- src/Components/Anchor/index.js
+++ src/Components/Anchor/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { btn } from '../Button/Button.module.scss';
 
 const Anchor = ({href,children}) => {
-    return <a className={btn} href={href} target="_blank">{children}</a>
+    return <a className={btn} href={href} target="_blank" rel="noopener noreferrer">{children}</a>
 }
 
 export default Anchor;
