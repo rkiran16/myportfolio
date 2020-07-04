7afabc08970c7ef2518767c8bbfc0cd6b023df70
Ravi Kiran Kanculakunta <kancravi@us.ibm.com>
2020-07-04 16:39:51 -0400

    Added icons and integrated scrollspy library

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Scrollspy from 'react-scrollspy'
 import { header, logo } from './Header.module.scss';
-import Logo from './logo.png';
 import Anchor from "../Anchor";
+
+// images
+import Logo from './logo.png';
+import Experience  from './icons/experience.png';
+import Work from './icons/work.png';
+import Skills from './icons/skills.png';
+import Contact from './icons/contact.png';
 
 const Header = () => {
     return(
@@ -9,15 +16,13 @@ const Header = () => {
             <div className={logo}>
                 <a href="/"><img src={Logo} alt="Portfolio Logo"/></a>
             </div>
-            <ul>
-                <li>Experience</li>
-                <li>Work</li>
-                <li>Skills</li>
-                <li>Contact</li>
-                <li>
-                    <Anchor href="./Resume.pdf" children={<span>Resume</span>}/>
-                </li>
-            </ul>
+            <Scrollspy items={ ['experience', 'work', 'skills', 'contact'] } currentClassName="is-current">
+                <li><a href="#experience"><img src={Experience} alt="Experience Logo"/> Experience</a></li>
+                <li><a href="#work"><img src={Work} alt="Work Logo"/> Work</a></li>
+                <li><a href="#skills"><img src={Skills} alt="Skills Logo"/>Skills</a></li>
+                <li><a href="#contact"><img src={Contact} alt="Contact Logo"/>Contact</a></li>
+                <li><Anchor href="./Resume.pdf" children={<span>Resume</span>}/></li>
+            </Scrollspy>
         </div>
     )
 }
