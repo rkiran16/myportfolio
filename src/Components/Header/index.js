d37e7ad39e6d8238471d5bd04a2f7734745dcd1a
Ravi Kiran Kanculakunta <kancravi@us.ibm.com>
2020-07-04 15:01:46 -0400

    valid href attribute

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -7,7 +7,7 @@ const Header = () => {
     return(
         <div className={header}>
             <div className={logo}>
-                <a href="#"><img src={Logo} alt="Portfolio Logo"/></a>
+                <a href="/"><img src={Logo} alt="Portfolio Logo"/></a>
             </div>
             <ul>
                 <li>Experience</li>
