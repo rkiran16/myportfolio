6164b81ee2854f4ecd9360aa078127b4895cea3a
Ravi Kiran Kanculakunta <kancravi@us.ibm.com>
2020-07-04 15:03:08 -0400

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.scss';
-import SideBar from "./SideBar";
-import Button from "./Components/Button";
 import Header from "./Components/Header";
 
 function App() {
