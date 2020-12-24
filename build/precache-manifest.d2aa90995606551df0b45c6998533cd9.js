ade640948de44d9bef15db86af3d662b10ad7188
Ravi Kiran Kanculakunta <kancravi@us.ibm.com>
2020-12-23 18:34:17 -0800

    updates

---
+++ build/precache-manifest.d2aa90995606551df0b45c6998533cd9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9999244ffa3ac0b4cf265c759e9dc1d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5bca735fd7109c6fe78",
+    "url": "/static/css/2.ac1ab58b.chunk.css"
+  },
+  {
+    "revision": "e6a133ac24e10ca64407",
+    "url": "/static/css/main.7f1acace.chunk.css"
+  },
+  {
+    "revision": "d5bca735fd7109c6fe78",
+    "url": "/static/js/2.993703fe.chunk.js"
+  },
+  {
+    "revision": "c687a54a8c2bb60ad0ba31e75fbd2c52",
+    "url": "/static/js/2.993703fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6a133ac24e10ca64407",
+    "url": "/static/js/main.2db68e43.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "e26661eca59b45c726b579fa228f5691",
+    "url": "/static/media/bookstore.e26661ec.png"
+  },
+  {
+    "revision": "36403accb5eac6aed58abbc57389c3f2",
+    "url": "/static/media/currybowl.36403acc.png"
+  },
+  {
+    "revision": "e964e6b01ef14a8511d4a4d2d3218488",
+    "url": "/static/media/learnCSS.e964e6b0.png"
+  },
+  {
+    "revision": "01e9c9d439aa0b8efadce249fe4457ff",
+    "url": "/static/media/logo.01e9c9d4.png"
+  },
+  {
+    "revision": "eaf04a703e122e5c7543d89cfd1e44d4",
+    "url": "/static/media/nyc.eaf04a70.png"
+  },
+  {
+    "revision": "651451b49b5613283860e17222884d23",
+    "url": "/static/media/profile.651451b4.jpg"
+  }
+]);
--- src/Work/index.js
+++ src/Work/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import cx from 'classnames';
-import {work, wrapper, projectDesc, wrapperReverse} from './work.module.scss';
+import {work, wrapper, grid} from './work.module.scss';
 import Heading from "../Components/Heading";
 import learnCSS from "./images/learnCSS.png";
 import curryBowl from "./images/currybowl.png";
+import nyc from "./images/nyc.png";
+import bookStore from "./images/bookstore.png";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faGithub} from '@fortawesome/free-brands-svg-icons'
 import {faExternalLinkAlt} from "@fortawesome/free-solid-svg-icons";
@@ -13,55 +15,70 @@ export default class Work extends React.Component {
         return (
             <section title="work" id="work" className={work}>
                 <Heading headingText="Work"/>
-                <div className={wrapper}>
-                    <figure>
-                        <img src={curryBowl} alt="Learn css"/>
-                        <figcaption>
-                            <p>
-                                <a href="https://github.com/rkiran16/restaurant" target="_blank"
-                                   rel="noopener noreferrer">
-                                    <FontAwesomeIcon icon={faGithub} size="2x"/>
-                                </a>
-                                <a href="https://rkiran16.github.io/restaurant"
-                                   target="_blank" rel="noopener noreferrer">
-                                    <FontAwesomeIcon icon={faExternalLinkAlt} size="2x"/>
-                                </a>
-                            </p>
-                        </figcaption>
-                    </figure>
-                    <div className={projectDesc}>
-                        <h4>Featured Project</h4>
-                        <h2>Curry Bowl</h2>
-                        <p>
-                            Responsive single page application built with React , GraphQL, Apollo Server. For
-                            Scaffolding
-                            used create-react-app.
-                        </p>
+                <div className={grid}>
+                    <div className={wrapper}>
+                        <figure>
+                            <img src={curryBowl} alt="Learn css"/>
+                            <figcaption>
+                                <p>
+                                    <a href="https://github.com/rkiran16/restaurant" target="_blank"
+                                       rel="noopener noreferrer">
+                                        <FontAwesomeIcon icon={faGithub} size="2x"/>
+                                    </a>
+                                    <a href="https://rkiran16.github.io/restaurant"
+                                       target="_blank" rel="noopener noreferrer">
+                                        <FontAwesomeIcon icon={faExternalLinkAlt} size="2x"/>
+                                    </a>
+                                </p>
+                            </figcaption>
+                        </figure>
                     </div>
-                </div>
-                <div className={cx(wrapper, wrapperReverse)}>
-                    <figure>
-                        <img src={learnCSS} alt="Learn css"/>
-                        <figcaption>
-                            <p>
-                                <a href="https://github.com/rkiran16/Clothing-Store" target="_blank"
-                                   rel="noopener noreferrer">
-                                    <FontAwesomeIcon icon={faGithub} size="2x"/>
-                                </a>
-                                <a href="http://parallax-clothing-store.s3-website-us-east-1.amazonaws.com/"
-                                   target="_blank" rel="noopener noreferrer">
-                                    <FontAwesomeIcon icon={faExternalLinkAlt} size="2x"/>
-                                </a>
-                            </p>
-                        </figcaption>
-                    </figure>
-                    <div className={projectDesc}>
-                        <h4>Featured Project</h4>
-                        <h2>BlackBird Clothing Store</h2>
-                        <p>
-                            Responsive single page application built using Vanila Js, CSS which demonstrates parallex
-                            and animations
-                        </p>
+                    <div className={cx(wrapper)}>
+                        <figure>
+                            <img src={learnCSS} alt="Learn css"/>
+                            <figcaption>
+                                <p>
+                                    <a href="https://github.com/rkiran16/Clothing-Store" target="_blank"
+                                       rel="noopener noreferrer">
+                                        <FontAwesomeIcon icon={faGithub} size="2x"/>
+                                    </a>
+                                    <a href="http://parallax-clothing-store.s3-website-us-east-1.amazonaws.com/"
+                                       target="_blank" rel="noopener noreferrer">
+                                        <FontAwesomeIcon icon={faExternalLinkAlt} size="2x"/>
+                                    </a>
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div className={cx(wrapper)}>
+                        <figure>
+                            <img src={nyc} alt="Help Desk"/>
+                            <figcaption>
+                                <p>
+                                    <a href="https://nydoe.us-east.mybluemix.net/"
+                                       target="_blank" rel="noopener noreferrer">
+                                        <FontAwesomeIcon icon={faExternalLinkAlt} size="2x"/>
+                                    </a>
+                                </p>
+                            </figcaption>
+                        </figure>
+                    </div>
+                    <div className={cx(wrapper)}>
+                        <figure>
+                            <img src={bookStore} alt="Book Store"/>
+                            <figcaption>
+                                <p>
+                                    <a href="https://github.com/rkiran16/infinitescroll" target="_blank"
+                                       rel="noopener noreferrer">
+                                        <FontAwesomeIcon icon={faGithub} size="2x"/>
+                                    </a>
+                                    <a href="https://rkiran16.github.io/infinitescroll/"
+                                       target="_blank" rel="noopener noreferrer">
+                                        <FontAwesomeIcon icon={faExternalLinkAlt} size="2x"/>
+                                    </a>
+                                </p>
+                            </figcaption>
+                        </figure>
                     </div>
                 </div>
             </section>
