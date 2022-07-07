4d9ca2f9a72a6ce418ba594a6fedc4fdf8727b8f
Ravi Kiran Kanculakunta <ravikancula@gmail.com>
2022-07-07 16:33:54 -0700

    Updates

--- src/Resume/index.js
+++ src/Resume/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Heading, Container, Box, Divider, Flex, Center, Spacer, Text, Button } from '@chakra-ui/react';
+import { Heading, Container, Box, Divider, Flex, Center, Spacer, Text,Link } from '@chakra-ui/react';
 import {
     Slider,
     SliderTrack,
@@ -102,13 +102,15 @@ export default class Resume extends React.Component {
                             <Skill skill="CSS" percentage="80" icon={<FontAwesomeIcon icon={faCss3} size="1x" />} />
                             <Skill skill="React" percentage="75" icon={<FontAwesomeIcon icon={faReact} size="1x" />} />
                             <Skill skill="Server Side" percentage="58" icon={<FontAwesomeIcon icon={faServicestack} size="1x" />} />
-                            <Button size='md' mb={12} height='60px' bg="white" 
+                            <Link size='md' mb={12} height='60px' bg="white" 
                                 borderRadius="34px"
                                 p={4}
+                                target="_blank"
+                                href="https://ogimg.s3.us-east-1.amazonaws.com/Resume.pdf?response-content-disposition=inline&X-Amz-Security-Token=IQoJb3JpZ2luX2VjELX%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLXdlc3QtMSJHMEUCIDJtn4RNwkfBPYkRVZhbLYZc83kREB6MSRgGPvgrom%2F0AiEA344CHOZ39eH2lvt5vmMGu%2Bk1JO%2FPV9z7rxT%2FSxLffv0q8QII3v%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FARABGgwwNTE0MjY1OTM1MDkiDFPnbUUjK6K75S%2BeoirFApkl50c9wuBeRc0vKzfv9kUiLRvm6W9baXHm8fnogkE%2FQolz%2FHag3m2FWZhm6e8rxc13d%2FldBI8VBRcW4%2Fy5%2Bi4ABa1n2zTV8y7tt14wgB2IKUoOXfluiCuYsG%2FEkQ05HzHwR1TZ%2FlWIVTX7GN3yr9qiwJdzjE0KCEN7VLz6YTg7YkYOZiVlmC9drX4HqvV8QWDf3%2BfnTY9acqbVeieZGsITJ3%2F%2B9kJNnnxfj%2BotMXP5Ce%2FEq1YWoADw%2FgAZRHwE9woMF9%2FFf37h2iisk8lbL02jthrXessm3B4NRnCnUKPZKLdYRtswVVlEBrWZuraPHOdiZQyrvfkMmEecenzMQGjfOH2qujf79dSkKtMgXJVTMqFW0dBQAwvBvDt9AhOPFraOEikEWZB%2BC1bXIhCUfBhYo3t0ygyVWK4qz%2BHflNWM9Nw9I%2BwwsIWdlgY6swKIKh6O8oRYK%2FBxOY7jY5GIzgQHUpXnkvYKJDglwclyMMiyOzAfvQT%2F%2BL%2BG%2Be9dfDuzLlOppfUVi6abIDf26gp2i6Bvjoi3j2swY1rYSeS9bbeyw3sCuitDiSoCc%2F%2FRWUoGbFh0%2BaQvDPWprSIEjKUsfTxOYh7lx%2BnSaEGxRj5ci8G%2FSET5EuuZ2Z1Nx0zbwdCukXqeMN79KKkR2CBMLSglreTIbBx75utYpHRcnKykyHgRckyGEmgqbhxeT8cRRtBvpgI1fCwvqYbgbqN99WTfYhDL93cic1GI89WHkDBL2CQWe%2FTh2AF24O6gYztYPUAwuw7knyCvmX%2FTacWD1enduvbN%2BlcE9IEE3RHHwPDjOLr%2FIqKVwzblDf04xWBsl85lOjigLGR%2FuiAQkMiZbhmKLorJ&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220707T233030Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIAQX6KDRLSXFBKGS7S%2F20220707%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=8e15368b41794299635fa18da40204cab873d58197369944b68244b591da4ca9"
                                 color="#aa1f00" width='200px' border='4px'
                                 borderColor='#aa1f00'>
                                 DOWNLOAD RESUME
-                            </Button>
+                            </Link>
                         </Box>
                     </Flex>
                 </Container>
