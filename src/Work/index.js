b2d76db809eb8a51bf98f8ef3fe44b53dc4fc940
Ravi Kiran Kanculakunta <ravikancula@gmail.com>
2022-07-07 08:48:33 -0700

    updates

--- src/Work/index.js
+++ src/Work/index.js
@@ -4,7 +4,7 @@ import { Box, Center, Heading } from "@chakra-ui/react";
 export default class Work extends React.Component {
     render() {
         return (
-            <Box w='100%' p={[4,8]} h="100%" bg="gray.50">
+            <Box w='100%' p={[4,8]} h="100%" bg="white">
                 <Center flexDirection="column">
                     <Heading as="h2" mb={12} size="2xl">RECENT <Box as="span" color="#aa1f00">WORK</Box></Heading>
                     <Box w="10%" mb={3} border='2px' borderColor='#aa1f00'/>
