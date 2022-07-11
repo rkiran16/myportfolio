e90c93de01b7d4d91ccc25c89f360d360701b5f6
Ravi Kiran Kanculakunta <ravikancula@gmail.com>
2022-07-10 23:54:38 -0700

    Updated Contact page

--- src/Contact/index.js
+++ src/Contact/index.js
@@ -1,37 +1,57 @@
 import React, {Component} from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { Box } from '@chakra-ui/react';
+import { Box, Heading, Flex, Stack, Text, Container } from '@chakra-ui/react';
+import Lottie from 'react-lottie';
+import animationData from './webDev.json'
 import {faGithub, faLinkedin} from '@fortawesome/free-brands-svg-icons';
 
+const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+    }
+};
+
 export default class Contact extends Component {
+
     render() {
         return (
-            <Box w="100%" h="100%" bg="gray.100">
-                <div>
-                    <h1>Get In Touch</h1>
-                </div>
-                <div>
-                    <div>
-                        {/* <img src={avatar} alt="Ravi Kanculakunta" /> */}
-                    </div>
-                    <a href="mailto:ravikancula@gmail.com">ravikancula@gmail.com</a>
-                    <ul>
-                        <li>
-                            <a href="https://www.linkedin.com/in/ravikancula/" target="_blank"
-                               rel="noopener noreferrer">
-                                <FontAwesomeIcon icon={faLinkedin} size="3x"/>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="https://github.com/rkiran16" target="_blank" rel="noopener noreferrer">
-                                <FontAwesomeIcon icon={faGithub} size="3x"/>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <span>&copy; Ravi Kanculakunta 2014</span>
-                </div>
+            <Box w="100%" h="100%"  p={[4,4,24]} bg="gray.100">
+                <Container maxW="8xl">
+                    <Flex w="100%" h="100%" flexDirection={["column-reverse", "column-reverse", "row"]} >
+                        <Flex w="60%" flexDirection="column" justifyContent="center" alignItems={["center", "center", "baseline"]}>
+                            <Box>
+                                <Heading as="h2" mb={10} size="2xl"><Box as="span" color="#aa1f00">Get In</Box> Touch</Heading>
+                            </Box>
+                            <Stack spacing={4}>
+                                <Stack spacing={1}>
+                                    <Text fontWeight="bold" fontSize='lg'>EMAIL ADDRESS</Text>
+                                    <Text ><a href="mailto:ravikancula@gmail.com">ravikancula@gmail.com</a></Text>
+                                </Stack>
+                                <Stack spacing={2}>
+                                    <Text fontWeight="bold" fontSize='lg'>SOCIAL</Text>
+                                    <Flex>
+                                    <Box mr={4}>
+                                            <a href="https://www.linkedin.com/in/ravikancula/"  target="_blank" rel="noopener noreferrer">
+                                            <FontAwesomeIcon className="white" icon={faLinkedin} size="2x"/>
+                                            </a>
+                                    </Box> 
+                                    <Box>
+                                            <a href="https://github.com/rkiran16" target="_blank" rel="noopener noreferrer">
+                                                <FontAwesomeIcon icon={faGithub} size="2x"/>
+                                            </a>
+                                    </Box>
+                                </Flex>
+                                </Stack>
+                          </Stack>    
+                        </Flex>
+                        <Box w="40%" h="100%"> 
+                             <Lottie options={defaultOptions} height={400}   width={400} />
+                        </Box>
+                    </Flex>
+                </Container>
             </Box>
         )
     }
