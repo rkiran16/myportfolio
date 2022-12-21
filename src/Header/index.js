3e1b73d6675e8514aad59fba6a4749139c78362c
Ravi Kanculakunta <ravikancula@gmail.com>
2022-12-21 14:37:43 -0800

    Updated Logo

--- src/Header/index.js
+++ src/Header/index.js
@@ -3,8 +3,8 @@ import React from "react";
 const Header = () => {
     return (
         <header className="sticky-top">
-            <nav id="navbar-example2" className="navbar border-bottom border-success border-4 bg-white  p-3">
-                <a className="navbar-brand" href="/">Navbar</a>
+            <nav id="navbar-example2" className="navbar border-bottom border-success border-4 bg-white  p-2">
+                <a className="display-4 logo" href="/">RK</a>
                 <ul className="nav nav-pills">
                     <li className="nav-item">
                         <a className="nav-link text-black fw-bold animate__animated animate__fadeInDown" href="/#intro">Intro</a>
