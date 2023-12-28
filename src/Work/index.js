77fea8d9bcd7f203ee3aa04e8c43b5225eeffd56
Ravi Kanculakunta <ravikancula@gmail.com>
2023-12-27 16:57:25 -0800

    Updated packages

--- src/Work/index.js
+++ src/Work/index.js
@@ -1,37 +1,52 @@
-import React from "react";
-import ProjectCard from "../components/ProjectCard";
-import workData from "./data";
+import React from 'react';
+import ProjectCard from '../components/ProjectCard';
+import workData from './data';
 
 const Work = () => {
-    return (
-        <div id="work" className="d-flex flex-column justify-content-center container px-3 min-vh-100 my-4 overflow-hidden">
-            <h1 className="mb-5 display-6 fw-bold" id="work"><u>My Work</u></h1>
-            <div className="row row-cols-1 row-cols-md-2 g-2 mb-4">
-                {workData && workData.map((work, index) => {
-                    const { id, title, image, date } = work;
-                    //const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-                    return (
-                        <>
-                            <div key={`${id}${index}`} className="col" data-bs-toggle="modal" data-bs-target={`#${id}Modal`}>
-                                <div className="project-card-wrapper">
-                                    <div className="position-absolute top-50 start-50 translate-middle">
-                                        <img src={image} className="img-fluid animate__animated shadow" alt={title}></img>
-                                    </div>
-                                    <div className="position-absolute w-100 bottom-0 p-2">
-                                        <div className="d-flex justify-content-between">
-                                            <h5 className="text-color7">{title}</h5>
-                                            <p className="mb-0 text-color7">{date}</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <ProjectCard work={work} />
-                        </>
-                    )
-                })}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div
+      id="work"
+      className="d-flex flex-column justify-content-center container px-3 min-vh-100 my-4 overflow-hidden"
+    >
+      <h1 className="mb-5 display-6 fw-bold" id="work">
+        <u>My Work</u>
+      </h1>
+      <div className="row row-cols-1 row-cols-md-2 g-2 mb-4">
+        {workData &&
+          workData.map((work, index) => {
+            const { id, title, image, date } = work;
+            //const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+            return (
+              <>
+                <div
+                  key={`${id}${index}`}
+                  className="col"
+                  data-bs-toggle="modal"
+                  data-bs-target={`#${id}Modal`}
+                >
+                  <div className="project-card-wrapper">
+                    <div className="position-absolute top-50 start-50 translate-middle">
+                      <img
+                        src={image}
+                        className="img-fluid animate__animated shadow"
+                        alt={title}
+                      ></img>
+                    </div>
+                    <div className="position-absolute w-100 bottom-0 p-2">
+                      <div className="d-flex justify-content-between">
+                        <h5 className="text-color7">{title}</h5>
+                        <p className="mb-0 text-color7">{date}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <ProjectCard work={work} />
+              </>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
 
 export default Work;
--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -1,51 +1,124 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const ProjectCard = ({ work }) => {
-  const { id, title, images, projectDescription, projectDetails } = work;
+  const { id, title, images, projectDescription, projectDetails } =
+    work;
   return (
-    <div className="modal fade" id={`${id}Modal`} tabindex="-1" aria-labelledby={`${id}ModalLabel`} aria-hidden="true">
+    <div
+      className="modal fade"
+      id={`${id}Modal`}
+      tabindex="-1"
+      aria-labelledby={`${id}ModalLabel`}
+      aria-hidden="true"
+    >
       <div className="modal-dialog modal-fullscreen-sm-down modal-dialog-centered modal-lg">
         <div className="modal-content">
           <div className="modal-header">
-            <h1 className="modal-title fs-3 text-color7" id={`${id}ModalLabel`}>{title}</h1>
-            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <h1
+              className="modal-title fs-3 text-color7"
+              id={`${id}ModalLabel`}
+            >
+              {title}
+            </h1>
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
           </div>
           <div className="modal-body p-0">
-            <div id={`carousel${id}Fade`} className="carousel slide carousel-fade">
+            <div
+              id={`carousel${id}Fade`}
+              data-bs-theme="dark"
+              className="carousel slide carousel-dark carousel-fade"
+            >
               <div className="carousel-inner">
-                {images && images.map((image, index) => {
-                  return (
-                    <div id={`${index}${id}`} className={`carousel-item border ${index === 1 ? 'active' : ''}`}>
-                      <img src={image} className="d-block w-100" alt="..."></img>
-                    </div>
-                  )
-                })}
+                {images &&
+                  images.map((image, index) => {
+                    return (
+                      <div
+                        id={`${index}${id}`}
+                        className={`carousel-item border ${
+                          index === 1 ? 'active' : ''
+                        }`}
+                      >
+                        <img
+                          src={image}
+                          className="d-block w-100"
+                          alt="..."
+                        ></img>
+                      </div>
+                    );
+                  })}
               </div>
-              <button className="carousel-control-prev" type="button" data-bs-target={`#carousel${id}Fade`} data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+              <button
+                className="carousel-control-prev"
+                type="button"
+                data-bs-target={`#carousel${id}Fade`}
+                data-bs-slide="prev"
+              >
+                <span
+                  className="carousel-control-prev-icon"
+                  aria-hidden="true"
+                ></span>
                 <span className="visually-hidden">Previous</span>
               </button>
-              <button className="carousel-control-next" type="button" data-bs-target={`#carousel${id}Fade`} data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+              <button
+                className="carousel-control-next"
+                type="button"
+                data-bs-target={`#carousel${id}Fade`}
+                data-bs-slide="next"
+              >
+                <span
+                  className="carousel-control-next-icon"
+                  aria-hidden="true"
+                ></span>
                 <span className="visually-hidden">Next</span>
               </button>
             </div>
             <div className="p-3">
-              <p className="mt-5 lh-base animate__animated animate__fadeInDown text-color7">{projectDescription}</p>
+              <p className="mt-5 lh-base animate__animated animate__fadeInDown text-color7">
+                {projectDescription}
+              </p>
               <div className="my-3 animate__animated animate__fadeInDown">
                 <dl className="row">
-                  {projectDetails && projectDetails.map((detail) => {
-                    return (
-                      <>
-                        <dt className="col-sm-3 text-color7">{detail?.name}</dt>
-                        <dd className="col-sm-9 d-flex text-color7">
-                          {detail?.value}
-                          {detail.urlText ? <a target="_blank" className="underline" rel="noopener noreferrer" href={detail.url}>{detail.urlText}</a> : ''}
-                          {detail.stack ? detail.stack.map((s) => <div className="d-flex justify-content-center bg-orange me-2 p-2"><FontAwesomeIcon className='text-white' icon={s} size='1x' /></div>) : ''}
-                        </dd>
-                      </>
-                    )
-                  })}
+                  {projectDetails &&
+                    projectDetails.map((detail) => {
+                      return (
+                        <>
+                          <dt className="col-sm-3 text-color7">
+                            {detail?.name}
+                          </dt>
+                          <dd className="col-sm-9 d-flex text-color7">
+                            {detail?.value}
+                            {detail.urlText ? (
+                              <a
+                                target="_blank"
+                                className="underline"
+                                rel="noopener noreferrer"
+                                href={detail.url}
+                              >
+                                {detail.urlText}
+                              </a>
+                            ) : (
+                              ''
+                            )}
+                            {detail.stack
+                              ? detail.stack.map((s) => (
+                                  <div className="d-flex justify-content-center bg-orange me-2 p-2">
+                                    <FontAwesomeIcon
+                                      className="text-white"
+                                      icon={s}
+                                      size="1x"
+                                    />
+                                  </div>
+                                ))
+                              : ''}
+                          </dd>
+                        </>
+                      );
+                    })}
                 </dl>
               </div>
             </div>
@@ -53,7 +126,7 @@ const ProjectCard = ({ work }) => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ProjectCard;
